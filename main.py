8a2193fa6bd855cda2684149ed6c4b29d0479524
KARIM GAHELRASOUL <s331391482@ocdsb>
2018-03-28 10:36:55 -0400

    message

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# <karim gahelrasoul>
 # March 28, 2018
 
 ###### uncomment this when you are ready to work on it
-#def CtoF ():
-#
+def CtoF (C):
+    F = 1.8* C+32
+    return F
 
-###### uncomment this when you are ready to work on it
-#def FtoC ():
-#
+def FtoC (F):
+ C = (0.55556) * (F-32)
+ return C
 
 temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+print(round(CtoF(temperature)))
+
+
